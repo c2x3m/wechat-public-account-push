c231a3f846519ef434204b35040b376d8308f043
c2x3m <117989350+c2x3m@users.noreply.github.com>
2022-11-11 20:38:04 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,7 +19,7 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '3i3i',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oTS7I5l4AQjHEetf7-kDxdvzU1OM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
@@ -55,7 +55,7 @@ const USER_CONFIG = {
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: '头疼了',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oTS7I5rADz3ukvjhP4cKsgpPphHc',
     }
